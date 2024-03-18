24eeb36287d1d68027c2af3554792c5c47e03c25
Mahabub Hossain <mahabubrishad@gmail.com>
2024-03-18 10:44:39 +0600

    Changes

---
+++ E-Com/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using E_Com.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Com.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Category {  get; set; }
+
+        private static readonly string[] separator = new String[] { @"bin\" };
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var env_name = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(separator, StringSplitOptions.None)[0];
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                 .AddJsonFile($"appsettings.{env_name}.json")
+                 .Build();
+                var config = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseMySql(config, ServerVersion.AutoDetect(config));
+            }
+        }
+    }
+}
---
+++ E-Com/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace E_Com.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
